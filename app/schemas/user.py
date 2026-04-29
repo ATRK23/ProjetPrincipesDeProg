ff1c506c91d1f1fcfaa6f867c966a2e640aa7f29
Arthur BAILLET <arthur.baillet@gmail.com>
2026-04-29 17:51:50 +0200

    config dict (remove pytest warning)

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 class UserBase(BaseModel):
     username: str
@@ -21,5 +21,4 @@ class UserUpdate(UserBase):
 class UserResponse(UserBase):
     id: int
     
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
